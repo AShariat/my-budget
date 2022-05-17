8152a3b0b81e55f4d2d0f345a5dc1a737d15b7f6
bullmoosedev <koleervine@gmail.com>
2022-05-16 18:08:14 -0600

    added css controls to login inputs

--- client/src/components/Testimonials/index.js
+++ client/src/components/Testimonials/index.js
@@ -27,7 +27,7 @@ function Testimonials(props) {
                     <div class="row">
                         <div class="col-md-4">
                             <div class="card mb-4 box-shadow">
-                                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style={heightWidthBlock} src='https://image.freepik.com/free-photo/smiling-banker-illustrating-benefit-bank-products_1262-15187.jpg' data-holder-rendered="true" />
+                                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style={heightWidthBlock} src='https://previews.123rf.com/images/fizkes/fizkes1907/fizkes190700121/126490052-happy-senior-old-family-couple-clients-make-financial-insurance-estate-business-deal-handshake-agent.jpg' data-holder-rendered="true" />
                                 <div class="card-body">
                                     <p class="card-text">This is one of the schmucks we scammed out of all his money-- I mean, one of our loyal customers, who's VERY happy with us!</p>
                                     <div class="d-flex justify-content-between align-items-center">
@@ -38,6 +38,35 @@ function Testimonials(props) {
                                 </div>
                             </div>
                         </div>
+
+                        <div class="col-md-4">
+                            <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style={heightWidthBlock} src='https://thumbs.dreamstime.com/z/happy-african-american-woman-holding-piggy-bank-sitting-couch-home-piggybank-saving-money-future-secure-herself-199966533.jpg' data-holder-rendered="true" />
+                                <div class="card-body">
+                                    <p class="card-text">This is one of the schmucks we scammed out of all his money-- I mean, one of our loyal customers, who's VERY happy with us!</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                            <button type="button" class="btn btn-sm btn-outline-secondary">Read his story!</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style={heightWidthBlock} src='https://thumbs.dreamstime.com/b/happy-smiling-asian-customer-old-man-showing-ok-happy-smiling-asian-customer-old-man-showing-ok-sign-young-salesman-closing-213507888.jpg' data-holder-rendered="true" />
+                                <div class="card-body">
+                                    <p class="card-text">This is one of the schmucks we scammed out of all his money-- I mean, one of our loyal customers, who's VERY happy with us!</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                            <button type="button" class="btn btn-sm btn-outline-secondary">Read his story!</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
 
                 </div>
