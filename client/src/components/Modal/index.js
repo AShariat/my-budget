bee53173c53ffa253be80432f1bf5143ae9131b5
bullmoosedev <koleervine@gmail.com>
2022-05-18 16:06:10 -0600

    added base bones for modal if user token is old or invalid

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -15,11 +15,11 @@ const UserInputModal = function () {
                         <form class="">
                             <div class="form-floating mb-3">
                                 <input type="email" class="form-control rounded-4" id="floatingInput" placeholder="name@example.com" />
-                                    <label for="floatingInput">Email address</label>
+                                <label for="floatingInput">Email address</label>
                             </div>
                             <div class="form-floating mb-3">
                                 <input type="password" class="form-control rounded-4" id="floatingPassword" placeholder="Password" />
-                                    <label for="floatingPassword">Password</label>
+                                <label for="floatingPassword">Password</label>
                             </div>
                             <button class="w-100 mb-2 btn btn-lg rounded-4 btn-primary" type="submit">Sign up</button>
                             <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small>
@@ -31,8 +31,33 @@ const UserInputModal = function () {
     );
 };
 
+const LoggedOutUserModal = function () {
+    return (
+        <div class="modal modal-sheet position-static d-block bg-secondary py-5" tabindex="-1" role="dialog" id="modalSheet">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content rounded-6 shadow">
+                    <div class="modal-header border-bottom-0">
+                        <h5 class="modal-title">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body py-0">
+                        <p>This is a modal sheet, a variation of the modal that docs itself to the bottom of the viewport like the newer share sheets in iOS.</p>
+                    </div>
+                    <div class="modal-footer flex-column border-top-0">
+                        <button type="button" class="btn btn-lg btn-primary w-100 mx-0 mb-2">Save changes</button>
+                        <button type="button" class="btn btn-lg btn-light w-100 mx-0" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
 export {
-    UserInputModal
+    UserInputModal,
+    LoggedOutUserModal
 };
 
 /* likely will make multiple modals for different input types */
+
+/* loggededoutusermodal for if a user auth token is invalid, reroute to login page */
