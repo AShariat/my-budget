7a80d2b2800ced43ba9a930f8066c4c872e00666
bullmoosedev <koleervine@gmail.com>
2022-05-20 15:26:16 -0600

    pie chart works and user dashboard closer to final product

--- client/src/components/PieGraph/index.js
+++ client/src/components/PieGraph/index.js
@@ -2,49 +2,8 @@ import React from 'react';
 
 const PieGraph = function () {
     return (
-        <div class="container my-4">
-
-            <p class="font-weight-bold">Learn how to create a Bootstrap Pie Chart and see the examples of proper implementation
-                and customization.
-
-            </p>
-
-            <p> this is a pie chart. you cannot eat it</p>
-
-            <hr class="mt-5" />
-
-            <p>Built with <a target="_blank" href="https://mdbootstrap.com/docs/standard/">Material Design for Bootstrap</a> - free and powerful Bootstrap UI KIT</p>
-
-            <a class="btn btn-primary me-2" href="https://mdbootstrap.com/docs/standard/getting-started/installation/" target="_blank" role="button">Download MDB UI KIT <i class="fas fa-download"></i></a>
-            <a class="btn btn-danger me-2" target="_blank" href="https://mdbootstrap.com/docs/standard/" role="button">Learn more</a>
-            <a class="btn btn-success me-2" target="_blank" href="https://mdbootstrap.com/docs/standard/getting-started/" role="button">Tutorials</a>
-            <a class="btn btn-dark me-2" target="_blank" href="https://github.com/mdbootstrap/mdb-ui-kit" role="button">GitHub <i class="fab fa-github ms-2"></i></a>
-
-            <hr class="mb-5" />
-
-            <div>
-                <canvas id="pieChart" style="max-width: 500px;"></canvas>
-            </div>
-
-        </div>
+        <div id='piechart'></div>
     );
 };
-
-/* //pie backend code for actual functionality
-    var ctxP = document.getElementById("pieChart").getContext('2d');
-    var myPieChart = new Chart(ctxP, {
-      type: 'pie',
-      data: {
-        labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
-        datasets: [{
-          data: [300, 50, 100, 40, 120],
-          backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-          hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
-        }]
-      },
-      options: {
-        responsive: true
-      }
-    }); */
 
 export default PieGraph;
