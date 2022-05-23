b0acc0ea498792a8ba083d5d2cae0f9dd0c218be
bullmoosedev <koleervine@gmail.com>
2022-05-23 15:23:40 -0600

    adding user query for transactions

--- client/utils/queries.js
+++ client/utils/queries.js
@@ -1,1 +1,17 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query me($username: String!) {
+    me(username: $username) {
+      username
+      email
+      transactions {
+        amount
+        category
+        subCat
+        createdAt
+        description
+      }
+    }
+  }
+`;
