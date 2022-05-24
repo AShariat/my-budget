0c2f9c29cb6c39f5b959548bf0f8d44aef4120f0
Amir Shariat <amirshariat1987@gmail.com>
2022-05-23 21:05:50 -0600

    new navbar working

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,40 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
 
-function Header(props) {
-
-  function showHeader() {
+function Header() {
+  function navRender() {
     if (Auth.loggedIn()) {
       return (
-        //   <a
-        //   className="nav-link"
-        //   href="/" onClick={() => Auth.logout()}>
-        //   Logout<span className="sr-only">(current)</span>
-        // </a>
-        <Link to="/" onClick={() => Auth.logout()}>Logout</Link>
-      );
-    } else {
-      return (
-        <Link to="/about">About</Link>,
-        <Link to="/testimonials">Testimonials</Link>,
-        <Link to="/login">Login</Link>,
-        <Link to="/signup">Signup</Link>
+        <nav className="text-center">
+          <Link to="/" onClick={() => Auth.logout()}>
+            Logout
+          </Link>
+        </nav>
       );
     }
+    return (
+      <nav className="text-center">
+        <Link to="/about">About Us</Link>
+        <Link to="/testimonials">Testimonials</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link>
+      </nav>
+    );
   }
 
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/">
-          <h1>
+        <h1>
+          <Link to="/">
             <i className="bi bi-currency-exchange"></i> My Budget
-          </h1>
-        </Link>
-        <nav className="text-center">
-          {showHeader()}
-        </nav>
+          </Link>
+        </h1>
+        {navRender()}
       </div>
     </header>
   );
