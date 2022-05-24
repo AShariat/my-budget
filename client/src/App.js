4a79b8582f2ff3e795b75c18049123a0420f7086
Amir Shariat <88262115+AShariat@users.noreply.github.com>
2022-05-24 02:00:38 -0600

    login page and signup page are created

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   ApolloProvider,
   ApolloClient,
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { setContext } from "@apollo/client/link/context";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import About from "./components/About";
 import Testimonials from "./components/Testimonials";
 import Dashboard from "./pages/Dashboard";
@@ -21,8 +22,18 @@ const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,40 +1,44 @@
 import React from "react";
-import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
 
-function Header() {
-  function navRender() {
-    if (Auth.loggedIn()) {
-      return (
-        <nav className="container d-flex">
-          <h4>
-            <Link to="/" onClick={() => Auth.logout()}>
-              Logout
-            </Link>
-          </h4>
-        </nav>
-      );
-    }
-    return (
-      <nav className="container d-flex">
-        <h4>
-          <Link to="/dashboard">Dashboard</Link>
-        </h4>
-        <h4>
-          <Link to="/about">About Us</Link>
-        </h4>
-        <h4>
-          <Link to="/testimonials">Testimonials</Link>
-        </h4>
-        <h4>
-          <Link to="/login">Login</Link>
-        </h4>
-        <h4>
-          <Link to="/signup">Signup</Link>
-        </h4>
-      </nav>
-    );
-  }
+const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  // function navRender() {
+  //   if (Auth.loggedIn()) {
+  //     return (
+  //       <nav className="container d-flex">
+  //         <h4>
+  //           <Link to="/" onClick={() => Auth.logout()}>
+  //             Logout
+  //           </Link>
+  //         </h4>
+  //       </nav>
+  //     );
+  //   }
+  //   return (
+  //     <nav className="container d-flex">
+  //       <h4>
+  //         <Link to="/dashboard">Dashboard</Link>
+  //       </h4>
+  //       <h4>
+  //         <Link to="/about">About Us</Link>
+  //       </h4>
+  //       <h4>
+  //         <Link to="/testimonials">Testimonials</Link>
+  //       </h4>
+  //       <h4>
+  //         <Link to="/login">Login</Link>
+  //       </h4>
+  //       <h4>
+  //         <Link to="/signup">Signup</Link>
+  //       </h4>
+  //     </nav>
+  //   );
+  // }
 
   return (
     <header className="container d-flex">
@@ -43,9 +47,36 @@ function Header() {
           <i className="bi bi-currency-exchange"></i> My Budget
         </Link>
       </h1>
-      {navRender()}
+      {/* {navRender()} */}
+      <nav className="text-center">
+        {Auth.loggedIn() ? (
+          <>
+            <h4>
+              <Link to="/dashboard">Dashboard</Link>
+            </h4>
+            <a href="/" onClick={logout}>
+              <h4>Logout</h4>
+            </a>
+          </>
+        ) : (
+          <>
+            <h4>
+              <Link to="/about">About Us</Link>
+            </h4>
+            <h4>
+              <Link to="/testimonials">Testimonials</Link>
+            </h4>
+            <h4>
+              <Link to="/login">Login</Link>
+            </h4>
+            <h4>
+              <Link to="/signup">Signup</Link>
+            </h4>
+          </>
+        )}
+      </nav>
     </header>
   );
-}
+};
 
 export default Header;
