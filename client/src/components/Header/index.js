48e79de8899a9ffaacafb155d62c47e322ee3bf7
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-05-23 20:29:27 -0600

    header renders if logged in

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
 
-const Header = () => {
+function Header(props) {
+
+  function showHeader() {
+    if (Auth.loggedIn()) {
+      return (
+        //   <a
+        //   className="nav-link"
+        //   href="/" onClick={() => Auth.logout()}>
+        //   Logout<span className="sr-only">(current)</span>
+        // </a>
+        <Link to="/" onClick={() => Auth.logout()}>Logout</Link>
+      );
+    } else {
+      return (
+        <Link to="/about">About</Link>,
+        <Link to="/testimonials">Testimonials</Link>,
+        <Link to="/login">Login</Link>,
+        <Link to="/signup">Signup</Link>
+      );
+    }
+  }
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -10,16 +32,12 @@ const Header = () => {
             <i className="bi bi-currency-exchange"></i> My Budget
           </h1>
         </Link>
-
         <nav className="text-center">
-          <Link to="/about">About</Link>
-          <Link to="/testimonials">Testimonials</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          {showHeader()}
         </nav>
       </div>
     </header>
   );
-};
+}
 
 export default Header;
